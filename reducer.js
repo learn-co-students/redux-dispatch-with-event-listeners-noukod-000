b6d119d904f48e6cf788ec01bda24e9afee484bf
Ricartho Franck <ricartho.franck@esih.edu>
2019-11-18 10:49:19 +0000

    Done.

--- reducer.js
+++ reducer.js
@@ -1,2 +1,29 @@
 // add code snippets from README
-dsd
+let state;
+
+function changeState(state = {count: 0},action){
+  switch(action.type){
+    case 'INCREASE':
+      return({...state,count: state.count+1})
+    default:
+      return state
+  }
+}
+
+function dispacth(action){
+  state = changeState(state,action)
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
